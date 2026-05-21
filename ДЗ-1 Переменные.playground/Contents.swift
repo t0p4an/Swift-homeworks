9002579f1d5b46ad47ee7cf600f7eea968d0f52f
Сергей Топченко <stopchenko@ivi.ru>
2026-05-21 18:28:37 +0300

    Второе задание по переменным

--- ДЗ-1 Переменные.playground/Contents.swift
+++ ДЗ-1 Переменные.playground/Contents.swift
@@ -8,3 +8,9 @@ var stringVar = "Hello"
 let boolLet = true
 var floatVar: Float = 10.5
 let doubleLet: Double = 10.5
+
+// Чем отличается UInt64 от UInt32:
+
+// UInt32 - занимает 32 бита памяти, UInt64 - 64 соответственно
+// В UInt 32 можно использовать числа от 0 до 4 294 967 295 (2³² - 1), в UInt 64 - от 0 до 18 446 744 073 709 551 615 (2⁶⁴ - 1)
+
