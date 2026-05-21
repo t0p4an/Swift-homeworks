b3032c15dbd1e935a82b6a2efe87c0a901f10c5c
Сергей Топченко <stopchenko@ivi.ru>
2026-05-21 18:08:39 +0300

    ДЗ - 1

---
+++ ДЗ-1 Переменные.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+// Создать свои переменные и константы разных типов. UInt, Int, String, Bool, Float, Double:
+
+var uintVar: UInt = 10
+let intLet: Int = -10
+var stringVar = "Hello"
+let boolLet = true
+var floatVar: Float = 10.5
+let doubleLet: Double = 10.5
